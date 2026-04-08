572147fe35492acc7f166072d408ec8784166016
ElioSpena <elio.spena.94@gmail.com>
2026-04-08 11:51:43 +0200

    feat: aggiunta funzione che incrementa quantità prodotto

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import { useState } from "react";
 function App() {
   const [addedProducts, setAddedProducts] = useState([]);
 
+  function updateProductQuantity(productName) {
+    setAddedProducts((prev) =>
+      prev.map((p) =>
+        p.name === productName ? { ...p, quantity: p.quantity + 1 } : p,
+      ),
+    );
+  }
+
   function addtoCart(product) {
-    if (addedProducts.find((p) => p.name === product.name)) return;
+    if (addedProducts.find((p) => p.name === product.name)) {
+      updateProductQuantity(product.name);
+      return;
+    }
     const newProd = { ...product, quantity: 1 };
     setAddedProducts([...addedProducts, newProd]);
   }
